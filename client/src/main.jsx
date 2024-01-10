66aa2b51289a241a38b889a7c304997454100d9f
hassan4702 <muhammadhassanshakil35066@gmail.com>
2024-01-10 21:56:21 +0500

    update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
-
-import { StateContextProvider } from './context';
-import App from './App';
-import './index.css';
-// 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import { NextUIProvider } from "@nextui-org/react";
+import { StateContextProvider } from "./context";
+import App from "./App";
+import "./index.css";
+//
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <ThirdwebProvider activeChain="goerli" clientId="67f9d109194f7343d2c9a621f0ef549f"> 
+  <ThirdwebProvider
+    activeChain="goerli"
+    clientId="67f9d109194f7343d2c9a621f0ef549f"
+  >
     <Router>
       <StateContextProvider>
-        <App/>
+        <NextUIProvider>
+          <App />
+        </NextUIProvider>
       </StateContextProvider>
     </Router>
-  </ThirdwebProvider> 
-)
+  </ThirdwebProvider>,
+);
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+const {nextui} = require("@nextui-org/react");
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -14,6 +16,7 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 }
 
