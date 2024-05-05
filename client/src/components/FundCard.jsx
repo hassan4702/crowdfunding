b239d7cf8c181e7c9650032c951d391df516d9d2
hassan4702 <muhammadhassanshakil35066@gmail.com>
2024-05-05 17:25:35 +0500

    Update image handling in FundCard component

--- client/src/components/FundCard.jsx
+++ client/src/components/FundCard.jsx
@@ -9,15 +9,15 @@ const FundCard = ({ owner, title,category, description, target, deadline, amount
   
   return (
     <div className="sm:w-[272px] w-full rounded-[15px] bg-[#1c1c24] cursor-pointer" onClick={handleClick}>
-      <MediaRenderer width='100%' height='158px' src={image} alt="fund" className="w-full h-[158px] object-cover rounded-[15px]"/>
-      <div className="flex flex-col p-4">
+      <MediaRenderer  className='rounded-[15px] overflow-hidden object-cover w-[100%]' width='100%' height='158px' src={image} alt="fund" />
+            <div className="flex flex-col p-4">
         <div className="flex flex-row items-center mb-[18px]">
           <img src={tagType} alt="tag" className="w-[17px] h-[17px] object-contain"/>
           <p className="ml-[12px] mt-[2px] font-epilogue font-mediuimgf;n yum text-[12px] text-[#808191]">{category}</p>
         </div>
 
         <div className="block">
-          <h3 className="font-epilogue font-semibold text-[16px] text-left leading-[26px] truncate">{title}</h3>
+          <h3 className="font-epilogue font-semibold text-[16px] text-left text-white leading-[26px] truncate">{title}</h3>
           <p className="mt-[5px] font-epilogue font-normal text-[#808191] text-left leading-[18px] truncate">{description}</p>
         </div>
 
